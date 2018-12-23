ff8dc30145b14b32b8767045ea8eb7545e5ed6ff
Lukáš Jirůšek <lukas.jirusek33@gmail.com>
2018-12-23 21:05:20 +0100

    dark theme

--- scripts/dark-theme.js
+++ scripts/dark-theme.js
@@ -1,19 +1,46 @@
 /*
 STRUCTURE:
-isSet= 'not set' | 1
-theme= white | black
+theme= [null] | white | black
 */
-
-if (localStorage.getItem('isSet') == null || undefined) {
-	localStorage.setItem('isSet', 1);
-	localStorage.setItem('theme', 'white');
-}
-
-// nastaveni barev podle aktualniho nastaveni
-function switchColor(){
-	localStorage.setItem('pes', 'jezevcik');
-	if(document.body.style.backgroundColor != "red"){document.body.style.backgroundColor = "red"}
+function setColor(){
+	if (localStorage.getItem('theme') == null || undefined) {
+		localStorage.setItem('theme', 'white');
+	}
 	else{
-		document.body.style.backgroundColor = "blue"
+		if(localStorage.getItem('theme') == 'white'){				/*White theme*/ 
+			document.body.style.backgroundColor = "white";
+			document.body.style.color = "black";
+			document.getElementById("changeColorButton").style.backgroundColor = "black";
+			document.getElementById("changeColorButton").style.color = "white";
+		}
+		else{
+			if(localStorage.getItem('theme') == 'black'){			/*Black theme*/
+				document.body.style.backgroundColor = "black";
+				document.body.style.color = "white";
+				document.getElementById("changeColorButton").style.backgroundColor = "white";
+				document.getElementById("changeColorButton").style.color = "black";
+			}
+			else{
+				alert("this should have had never happened");
+			}
+		}
 	}
 }
+function switchColor(){
+	document.getElementById("changeColorButton").backgroundColor = "red";
+	document.getElementById("changeColorButton").color = "blue";
+	if(localStorage.getItem('theme') == 'white'){
+		localStorage.setItem('theme', 'black');
+		setColor();
+	}
+	else{
+		if(localStorage.getItem('theme') == 'black'){
+			localStorage.setItem('theme', 'white');
+			setColor();
+		}
+		else{
+			alert("this should have had never happened as well");
+		}
+	}
+}
+window.onload = setColor();
